d6c530d7a64a0e59aa03b32c3432407f7fcded7f
Shashwat Sinha <shashwatsinha1999@gmail.com>
2022-04-01 00:07:45 +0530

    Time: 2 ms (95.60%), Space: 42.6 MB (78.09%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.java
+++ 93-restore-ip-addresses/93-restore-ip-addresses.java
@@ -1,62 +1,53 @@
 class Solution {
-  int n;
-  String s;
-  LinkedList<String> segments = new LinkedList<String>();
-  ArrayList<String> output = new ArrayList<String>();
-
-  public boolean valid(String segment) {
-    /*
-    Check if the current segment is valid :
-    1. less or equal to 255      
-    2. the first character could be '0' 
-    only if the segment is equal to '0'
-    */
-    int m = segment.length();
-    if (m > 3)
-      return false;
-    return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
-  }
-
-  public void update_output(int curr_pos) {
-    /*
-    Append the current list of segments 
-    to the list of solutions
-    */
-    String segment = s.substring(curr_pos + 1, n);
-    if (valid(segment)) {
-      segments.add(segment);
-      output.add(String.join(".", segments));
-      segments.removeLast();
+    
+    int n;
+    String s;
+    List<String> res=new ArrayList<>();
+    LinkedList<String> segments=new LinkedList<>();
+    
+    public List<String> restoreIpAddresses(String s) {
+        this.s=s;
+        n=s.length();
+        
+        backtrack(-1,3);
+        
+        return res;
     }
-  }
-
-  public void backtrack(int prev_pos, int dots) {
-    /*
-    prev_pos : the position of the previously placed dot
-    dots : number of dots to place
-    */
-    // The current dot curr_pos could be placed 
-    // in a range from prev_pos + 1 to prev_pos + 4.
-    // The dot couldn't be placed 
-    // after the last character in the string.
-    int max_pos = Math.min(n - 1, prev_pos + 4);
-    for (int curr_pos = prev_pos + 1; curr_pos < max_pos; curr_pos++) {
-      String segment = s.substring(prev_pos + 1, curr_pos + 1);
-      if (valid(segment)) {
-        segments.add(segment);  // place dot
-        if (dots - 1 == 0)      // if all 3 dots are placed
-          update_output(curr_pos);  // add the solution to output
-        else
-          backtrack(curr_pos, dots - 1);  // continue to place dots
-        segments.removeLast();  // remove the last placed dot 
-      }
+    
+    private void backtrack(int prevPos,int dots){
+        int maxPos=Math.min(prevPos+4,n-1);
+        
+        for(int currPos=prevPos+1;currPos<maxPos;currPos++){
+            String segment=s.substring(prevPos+1,currPos+1);
+            if(valid(segment)){
+                segments.add(segment); // Add dot...
+                if(dots-1==0){ // If all 3 dots are placed...
+                    update_output(currPos);
+                }else{
+                    backtrack(currPos,dots-1);
+                }
+                segments.removeLast();
+            }
+        }
     }
-  }
-
-  public List<String> restoreIpAddresses(String s) {
-    n = s.length();
-    this.s = s;
-    backtrack(-1, 3);
-    return output;
-  }
+    
+    private boolean valid(String segment){
+        int m=segment.length();
+        if(m>3){
+            return false;
+        }
+        return (segment.charAt(0)!='0') ? Integer.parseInt(segment)<=255: (m==1);
+    }
+    
+    private void update_output(int currPos){
+        String segment=s.substring(currPos+1,n);
+        
+        if(valid(segment)){
+            segments.add(segment);
+            res.add(String.join(".",segments));
+            segments.removeLast();
+        }
+        
+    }
+    
 }
