cb9d0f74c4b411c560706fadbfc740e02a3eb3fa
Shashwat Sinha <shashwatsinha1999@gmail.com>
2022-10-06 21:49:45 +0530

    Time: 1 ms (97.63%), Space: 43.4 MB (99.50%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,16 +3,17 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        // Check if the first row has a Zero...
-        boolean isFirstRowZero = false;
-        for (int j=0;j<m;j++) {
-            if (matrix[0][j] == 0) {
-                isFirstRowZero = true;
+        boolean firstRowContainsZero = false;
+        
+        // Check if the first row contains a zero.
+        for (int i=0;i<m;i++) {
+            if (matrix[0][i] == 0) {
+                firstRowContainsZero = true;
             }
         }
         
-        // Now fill the cells of first row with Zero if the correspoding column has a zero...
-        for (int i=0;i<n;i++) {
+        // Mark the cells of first row as zero which have a zero in their column.
+        for (int i=1;i<n;i++) {
             for (int j=0;j<m;j++) {
                 if (matrix[i][j] == 0) {
                     matrix[0][j] = 0;
@@ -20,7 +21,7 @@ class Solution {
             }
         }
         
-        // Traverse over the remaining matrix and mark the cells...
+        // Now visit all the cells except the cells of the first row and mark them accordingly.
         for (int i=1;i<n;i++) {
             boolean rowContainsZero = false;
             for (int j=0;j<m;j++) {
@@ -28,6 +29,7 @@ class Solution {
                     rowContainsZero = true;
                 }
             }
+            
             for (int j=0;j<m;j++) {
                 if (rowContainsZero || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
@@ -35,10 +37,10 @@ class Solution {
             }
         }
         
-        // Now mark the required cells of row 0 with Zeros...
-        for (int j=0;j<m;j++) {
-            if (isFirstRowZero || matrix[0][j] == 0) {
-                matrix[0][j] = 0;
+        // Now mark the unvisited first row accordingly.
+        for (int i=0;i<m;i++) {
+            if (firstRowContainsZero) {
+                matrix[0][i] = 0;
             }
         }
     }
