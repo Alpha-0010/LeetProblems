4dd8cd3086e1b62d82b9023855ec8a4ea67edbbc
Shashwat Sinha <shashwatsinha1999@gmail.com>
2022-10-08 00:35:14 +0530

    Time: 2 ms (24.53%), Space: 42.5 MB (93.37%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,19 +1,19 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int cnt = 0;
+        // int cnt = 0;
         int n = nums.length;
         
-        for (int i=1;i<n;i++) {
-            if (nums[i-1] >= nums[i]) {
-                cnt++;
-            }
-        }
+//         for (int i=1;i<n;i++) {
+//             if (nums[i-1] >= nums[i]) {
+//                 cnt++;
+//             }
+//         }
         
-        // Check if all the elements are in descending order.
-        if (cnt == nums.length -1) {
-            Arrays.sort(nums);
-            return;
-        }
+//         // Check if all the elements are in descending order.
+//         if (cnt == nums.length -1) {
+//             Arrays.sort(nums);
+//             return;
+//         }
         
         // Determine the first element from the right which is less than the next element.
         int idx1 = -1;
@@ -26,16 +26,20 @@ class Solution {
         
         // Determine the element which is greater than nums[idx1] and swap them. 
         int idx2 = -1;
-        for (int i=n-1;i>=0;i--) {
-            if (nums[i] > nums[idx1]) {
-                idx2 = i;
-                break;
+        if (idx1 != -1) {
+            for (int i=n-1;i>=0;i--) {
+                if (nums[i] > nums[idx1]) {
+                    idx2 = i;
+                    break;
+                }
             }
         }
         
-        int temp = nums[idx1];
-        nums[idx1] = nums[idx2];
-        nums[idx2] = temp;
+        if (idx1 != -1) {
+            int temp = nums[idx1];
+            nums[idx1] = nums[idx2];
+            nums[idx2] = temp;
+        }
         // Finally sort the array from idx+1
         Arrays.sort(nums,idx1+1,nums.length);
     }
